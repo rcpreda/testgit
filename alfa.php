64959b301ddb4bd1072065e2645947684a1c3fb6
Razvan Preda <rcpreda@gmail.com>
2017-02-11 08:56:58 +0000

    WIP 1

--- alfa.php
+++ alfa.php
@@ -4,8 +4,13 @@
 class Alfa {
 
 	protected $echo;
-
+        
+	/**
+	 * @var int $echo 
+	 */
 	public function alfatest($echo) {
 		$this->echo = $echo;
 	}
 }
+
+//end of line 
--- practice.php
+++ practice.php
@@ -3,13 +3,16 @@
 //text on the top of the file 
 
 class Foobar {
-
+	
+	/**
+	 * This is a cnstructor documentation
+	 */
 	public function __construct() {
 		
 	}
 
 }
 
-
+//add some stra text
 //text on the bottom of the file
   
